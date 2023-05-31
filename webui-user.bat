4fcfdc7e660b9f3101d337b22cc70f4691e659aa
zixzaxlad <45975299+zixzaxlad@users.noreply.github.com>
2023-05-31 19:07:29 +0800

    Update webui-user.bat

--- webui-user.bat
+++ webui-user.bat
@@ -3,7 +3,7 @@
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS= --xformers --api --autolaunch --theme=dark
+set COMMANDLINE_ARGS= --xformers --api --autolaunch --theme=dark  --no-half --disable-nan-check
 
 git pull
 call webui.bat
